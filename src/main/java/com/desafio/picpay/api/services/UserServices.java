86c0706a70caceb9bf11c29ae36b877761a4fa31
Douglinhas <daguiaralcantara@gmail.com>
2023-10-31 22:07:36 -0300

    feat: create user service

---
+++ src/main/java/com/desafio/picpay/api/services/UserServices.java
@@ -0,0 +1,35 @@
+package com.desafio.picpay.api.services;
+
+import com.desafio.picpay.api.domain.user.User;
+import com.desafio.picpay.api.domain.user.UserType;
+import com.desafio.picpay.api.models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário  do tipo lojista não está autorizado a realizar transações.");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw  new Exception("Saldo insuficiente.");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+
+}
