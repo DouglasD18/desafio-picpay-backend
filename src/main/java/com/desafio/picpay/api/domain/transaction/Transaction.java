d03dc4f72a5f8dc583d0e4b965524297dfe42c0d
Douglinhas <daguiaralcantara@gmail.com>
2023-10-31 21:50:46 -0300

    feat: create transaction domain

---
+++ src/main/java/com/desafio/picpay/api/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.desafio.picpay.api.domain.transaction;
+
+import com.desafio.picpay.api.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timeStamp;
+}
